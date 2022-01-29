4a8c7b68b1ccab28143019bb4209c0dae9f6338f
Roland Coeurjoly <rolandcoeurjoly@gmail.com>
2022-01-29 18:08:49 +0100

    WIP

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "graphtage";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/d9e21f284317f85b3476c0043f4efea87a226c3a";
+
+  outputs = { self, nixpkgs }:
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      myPoetryEnv = pkgs.poetry2nix.mkPoetryEnv {
+        pyproject = ./pyproject.toml;
+        poetrylock = ./poetry.lock;
+      };
+    in {
+      devShell.x86_64-linux = myPoetryEnv;
+    };
+}
